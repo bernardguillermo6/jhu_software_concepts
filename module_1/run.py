6a4a13e77fdc4eae781c6aaf86da86075dbd441e
Bernard Guillermo <bernard@fieldguide.io>
2025-08-29 19:57:22 -0700

    adding latest files

---
+++ module_1/run.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from app import create_app
+
+app = create_app()  # create Flask app instance
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=8000)
