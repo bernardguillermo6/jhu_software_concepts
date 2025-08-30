e140eab891ff07915d07ab9f8eb276b49aa21370
Bernard Guillermo <bernard@fieldguide.io>
2025-08-29 20:39:06 -0700

    pushing latest files

--- module_1/run.py
+++ module_1/run.py
@@ -4,4 +4,4 @@ from app import create_app
 app = create_app()  # create Flask app instance
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8000)
+    app.run(debug=True, host="127.0.0.1", port=8080)
